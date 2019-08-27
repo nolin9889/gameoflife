51af58e5ef2a688e300f860ce75759c82ae41565
Noel Lin <nolin9889@gmail.com>
2019-08-26 18:41:54 -0700

    Got the neighbor counting logic working

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      grid: this.generateGrid(5,5),
+      grid: this.generateGrid(5,8),
       date: new Date(),
       running: false,
     }
@@ -28,9 +28,14 @@ class App extends React.Component {
   }
 
   toggle() {
-    this.setState({
-      running: !this.state.running
-    })
+    // this.setState({
+    //   running: !this.state.running
+    // })   
+    
+      this.setState({
+        grid: this.updateGrid(this.state.grid)
+      });
+
   }
 
   tick() {
@@ -80,48 +85,47 @@ class App extends React.Component {
     // let y = Math.floor(Math.random() * Math.floor(grid[0].length))
     // grid[x][y] = !grid[x][y];
 
+    // Initialize neighborCount 2d array with 0
+    let neighborCount = new Array(grid.length);
+    for (var i = 0; i < grid.length; i++) {
+      neighborCount[i] = new Array(grid[0].length);
+    }
+    for(let i = 0; i < grid.length; i++) {
+      for (let j = 0; j < grid[0].length; j++) {
+        neighborCount[i][j] = 0;
+      }
+    }
+    // Count how many neighbors there are in each cell.
+    for (let u = 0; u < grid.length; u++) {
+      console.log(u);
+      for (let v = 0; v < grid[u].length; v++) {
+        console.log("grid[u].length: " + grid[u].length);
+        neighborCount[u][v] = this.getNeighborsCount(grid, u, v);
+      }
+    }
+    neighborCount.forEach(row => console.log(row));
+
     // For a space that is 'populated':
     // Each cell with one or no neighbors dies, as if by solitude.
     // Each cell with four or more neighbors dies, as if by overpopulation.
     // Each cell with two or three neighbors survives.
     // For a space that is 'empty' or 'unpopulated'
     // Each cell with three neighbors becomes populated.
-    let neighbors = new Array(grid.length);
-    
-    for (var i = 0; i < grid.length; i++) {
-      neighbors[i] = new Array(grid[0].length);
-    }
-    // Initialize data.
-    for(let i = 0; i < grid.length; i++) {
-      for (let j = 0; j < grid[0].length; j++) {
-        neighbors[i][j] = 0;
-      }
-    }
-
-    for (let i = 0; i < grid.length; i++) {
-      for (let j = 0; j < grid[i].length; j++) {
-        neighbors[i][j] = this.getNeighborsCount(grid, i, j);
-        console.log(neighbors[i][j]);
-      }
-
-    for (let x = 0; x < grid.length; x++) {
-      for (let y = 0; y < grid[x].length; y++) {
+    for (let r = 0; r < grid.length; r++) {
+      for (let c = 0; c < grid[r].length; c++) {
         
-        if (grid[x][y] === false && neighbors[x][y] === 3) {
-          grid[x][y] = true;
+        if (neighborCount[r][c] === 3) {
+          grid[r][c] = true;
         }
-
-        else if (neighbors[x][y] < 2 || neighbors[x][y] > 4) {
-          grid[x][y] = false;
+        else if (neighborCount[r][c] < 2 || neighborCount[r][c] > 4) {
+          grid[r][c] = false;
         }
-
-        else {
-          // Do nothing. Remain the same if cell has 2 or 3 neighbors. 
-        }
+        else {} // Do nothing if cell has 2 neighbors.
+        
       }
     }  
 
-      }
+      
     
     
     return grid;
